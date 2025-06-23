4b9fd8f956cf27d1f9586d38f0b42fadc69a8165
tienphan <tienvpse@gmail.com>
2025-06-24 00:21:55 +0700

    chore: format doc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,12 @@ import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) { }
 
 	@Get()
 	getHello() {
 		return this.appService.getHello();
 	}
-
 
 	@Get("/new")
 	getNewHello() {
